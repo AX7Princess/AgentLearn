d730c5d6ecd65a24a6c1977b21754f4473748617
AX7Princess <a1795072882@163.com>
2026-08-10 23:48:37 +0800

    fc

--- baseloop.py
+++ baseloop.py
@@ -1,5 +1,6 @@
 import json
 from openai import OpenAI
+import os
 def get_weather(city:str)->str: 
     #天气实际查询函数
     return f"{city}"+"今天台风，注意安全"
@@ -20,10 +21,27 @@ tools=[
             }
         }  
 ]
-messages=[{"role":"suer","content":"今天北京天气怎么样"}]
-client=OpenAI(api_key=os.getenv(),base_url="https://api.deepseek.com")
+messages=[{"role":"user","content":"今天北京天气怎么样"}]
+client=OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"),base_url="https://api.deepseek.com")
 res=client.chat.completions.create(
-    model="deep-seek-v4-flash",
+    model="deepseek-v4-flash",
     messages=messages,
-    max_tookens=100,
+    tools=tools,
+    max_tokens=100,
 )
+msg=res.choices[0].message
+try:
+    print("msg:type",type(msg),"\n\n",msg)
+except:
+    print("无法打印")
+if msg.tool_calls:
+    messages.append(msg)
+    for tc in msg.tool_calls:
+        result=get_weather(**json.loads(tc.function.arguments))
+        try:
+            print("result:"+result+"\n")
+        except Exception as e:
+            print("result无法打印",e)
+        messages.append({"role":"tool","tool_call_id":tc.id,"content":result})
+    final=client.chat.completions.create(model="deepseek-v4-flash", messages=messages, tools=tools)
+    print(final.choices[0].message.content)
